61f4c01d4facb092c77c02f6a80c7d15e0c19aba
hexhowells <58524460+hexhowells@users.noreply.github.com>
2025-09-13 22:56:34 +0100

    add load_checkpoint abstract method

--- slate/slate/agent.py
+++ slate/slate/agent.py
@@ -5,5 +5,24 @@ import numpy as np
 class Agent(ABC):
 	@abstractmethod
 	def get_action(self, frame: np.ndarray) -> int:
-		"""Get next action given current frame"""
+		"""
+		Get next action given current frame
+
+		Args:
+			frame: the latest environment frame
+
+		Returns:
+			the numerical action code
+		"""
+		pass
+
+
+	@abstractmethod
+	def load_checkpoint(self, checkpoint: str) -> None:
+		"""
+		Load checkpoint from a given checkpoint file
+
+		Args:
+			checkpoint: the filepath to load the checkpoint from
+		"""
 		pass
