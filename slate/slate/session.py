3f5e05042eb99c9be7753fddc533394c97577ff9
hexhowells <58524460+hexhowells@users.noreply.github.com>
2025-11-12 22:58:39 +0000

    add asset attribute to Session

--- slate/slate/session.py
+++ slate/slate/session.py
@@ -3,7 +3,8 @@ import threading
 
 class Session:
     def __init__(self, sid: str):
-        self.sid = sid
+        self.sid: str = sid
+        self.asset: dict[str, str|int|list] = {}
         self.cursor: int = 0
         self.paused: bool = True
         self.lock = threading.Lock()
