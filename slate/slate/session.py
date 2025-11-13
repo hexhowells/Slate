371319a33b78ead1bc80ffaf57edc777853fbbec
hexhowells <58524460+hexhowells@users.noreply.github.com>
2025-11-13 20:39:35 +0000

    add attributes to Session class

--- slate/slate/session.py
+++ slate/slate/session.py
@@ -7,4 +7,7 @@ class Session:
         self.asset: dict[str, str|int|list] = {}
         self.cursor: int = 0
         self.paused: bool = True
+        self.awaiting_ack: bool = False
+        self.streaming: bool = False
+        self.last_sent_cursor: int | None = None
         self.lock = threading.Lock()
