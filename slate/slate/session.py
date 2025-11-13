a10eadaea59423281197ce14baad759b90f48fa1
hexhowells <58524460+hexhowells@users.noreply.github.com>
2025-11-13 21:47:15 +0000

    update asset dict value to Any to resolve typing checks for now

--- slate/slate/session.py
+++ slate/slate/session.py
@@ -1,10 +1,11 @@
 import threading
+from typing import Any
 
 
 class Session:
     def __init__(self, sid: str):
         self.sid: str = sid
-        self.asset: dict[str, str|int|list] = {}
+        self.asset: dict[str, Any] = {}
         self.cursor: int = 0
         self.paused: bool = True
         self.awaiting_ack: bool = False
