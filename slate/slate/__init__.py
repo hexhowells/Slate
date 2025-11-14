5b63a500fa872cc24542f14ca43c1dd0aa2a8b9e
hexhowells <58524460+hexhowells@users.noreply.github.com>
2025-11-14 21:37:42 +0000

    update __init__.py files

--- slate/slate/__init__.py
+++ slate/slate/__init__.py
@@ -2,3 +2,5 @@ from .slate import SlateClient
 from .agent import Agent
 from .utils import FrameBuffer
 from .schemas import frame_payload
+
+__all__ = ["SlateClient", "Agent", "FrameBuffer", "frame_payload"]
--- slate/slate/schemas/__init__.py
+++ slate/slate/schemas/__init__.py
@@ -1,1 +1,4 @@
 from .frame_payload import FramePayload, EnvInfo
+from . import frame_payload
+
+__all__ = ["FramePayload", "EnvInfo", "frame_payload"]
